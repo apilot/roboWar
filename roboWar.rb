cb86621ac61a785fda8b84dcf608c40f20919b87
Alex Boyarinov <a.boyarinov@gmail.com>
2020-05-04 19:14:27 +0400

    add healthy to robots and damage in attack

--- roboWar.rb
+++ roboWar.rb
@@ -1,17 +1,23 @@
 # init arays
   # command 1
-  @arr1 = Array.new(10,1)
+  @arr1 = Array.new(10,100)
   # command 2
-  @arr2 = Array.new(10,1)
+  @arr2 = Array.new(10,100)
 # ataka
   #method for attacks
 
   def attack(arr)
     sleep 1
     i = rand(0..9)
-    if arr[i] == 1
-      arr[i] = 0
-      puts "Робот номер #{i} уничтожен"
+    if arr[i] >= 1
+      h = rand(30..70)
+      arr[i] -= h
+      if arr[i] < 1
+        arr[i] = 0
+        puts "Робот номер #{i} уничтожен"
+      else
+        puts "Робот номер #{i} ранен, осталось #{arr[i]}% жизни"
+      end
     else
       puts "Промахнулись, под номером: #{i} никого нет"
     end
@@ -19,23 +25,23 @@
   end
 # check_victory
   def victory?
-    robots_left1 = @arr1.count { |x| x==1 }
-    robots_left2 = @arr2.count { |x| x==1 }
+    robots_left1 = @arr1.count { |x| x>=1 }
+    robots_left2 = @arr2.count { |x| x>=1 }
 
     if robots_left1 ==0
-      puts "Команда 2 победила, в команде осталось #{robots_left2} роботов"
+      puts "Команда 2 победила, в команде осталось #{robots_left2} робота(ов)"
       return true
     end
     if robots_left2 ==0
-      puts "Команда 1 победила, в команде осталось #{robots_left1} роботов"
+      puts "Команда 1 победила, в команде осталось #{robots_left1} робота(ов)"
       return true
     end
     false
   end
 # statistics
 def stats
-  cnt1 = @arr1.count { |x| x == 1 }
-  cnt2 = @arr2.count { |x| x == 1 }
+  cnt1 = @arr1.count { |x| x >= 1 }
+  cnt2 = @arr2.count { |x| x >= 1 }
   puts "1 команда: #{cnt1} роботов в строю"
   puts "2 команда: #{cnt2} роботов в строю"
 end
@@ -45,13 +51,13 @@ loop do
   attack(@arr2)
   exit if victory?
   stats
-  sleep 3
+  sleep 1
   puts
 
   puts 'Вторая команда наносит удар..'
   attack(@arr1)
   exit if victory?
   stats
-  sleep 3
+  sleep 1
   puts
 end
