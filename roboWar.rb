d4fd73df0bf7c5b31d3a23d6c2fa96e2ba765a64
Alex Boyarinov <a.boyarinov@gmail.com>
2020-05-04 18:49:19 +0400

    create and dcbe method attac

--- roboWar.rb
+++ roboWar.rb
@@ -4,7 +4,19 @@
   # command 2
   @arr2 = Array.new(10,1)
 # ataka
+  #method for attacks
 
+  def attack(arr)
+    sleep 1
+    i = rand(0..9)
+    if arr[i] == 1
+      arr[i] = 0
+      puts "Робот номер #{i} уничтожен"
+    else
+      puts "Промахнулись, под номером: #{i} никого нет"
+    end
+    sleep 1
+  end
 # check_victory
 
 # statistics
