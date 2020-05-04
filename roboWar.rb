91b82a97956f922759f7c56f812d5ccaf25e48c7
Alex Boyarinov <a.boyarinov@gmail.com>
2020-05-04 18:53:48 +0400

    add method check victory

--- roboWar.rb
+++ roboWar.rb
@@ -18,7 +18,20 @@
     sleep 1
   end
 # check_victory
+  def victory?
+    robots_left1 = @arr1.count { |x| x==1 }
+    robots_left2 = @arr2.count { |x| x==1 }
 
+    if robots_left1 ==0
+      puts "Команда 2 победила, в команде осталось #{robots_left2} роботов"
+      return true
+    end
+    if robots_left2 ==0
+      puts "Команда 1 победила, в команде осталось #{robots_left1} роботов"
+      return false
+    end
+    false
+  end
 # statistics
 
 # main
